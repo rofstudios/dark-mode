2141800cce0e97adbeb2656b4d38c225e7c118c0
Your Name <you@example.com>
2020-03-12 09:48:27 -0400

    added usedarkmode.js hook, adding return to side effect

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,6 +1,21 @@
 import { useLocalStorage } from './useLocalStorage';
+import { useEffect } from 'react';
 
 let useDarkMode = (key, initialValue) => {
-    let [values, setValues] = useLocalStorage(key, initialValue);
-    
+    let [useDM, setUseDM] = useLocalStorage('darkMode', false);//initial value of page on load
+    let body = document.getElementsByTagName('body');
+
+    useEffect(() => {
+        if ( useDM = true){
+            body.classList.add('dark-mode')
+        }else {
+            body.classList.remove('dark-mode')
+        }
+    }, [useDM])
+
+    return [ useDM, setUseDM ]
+    //must return these values /setter notetoself*****************************************
+
 }
+
+export default useDarkMode;
